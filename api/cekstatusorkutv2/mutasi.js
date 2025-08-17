2a708a75696f37d8f78baf3c7ffd269efc668c44
AldiXD-HACK <aldixd2009@gmail.com>
2025-08-17 08:40:19 +0700

    Update mutasi.js
    Signed-off-by: AldiXD-HACK <aldixd2009@gmail.com>

--- api/cekstatusorkutv2/mutasi.js
+++ api/cekstatusorkutv2/mutasi.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 
 // Configuration constants
 const OrderKuota = {
-  KASIR_HOST: '443',
+  KASIR_HOST: 'orderkuota.com',
   MEBVIEW_USER_AGENT: 'Mozilla/5.0 (Linux; Android 10; M2004J19C Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.185 Mobile Safari/537.36',
-  APP_PACKAGE: 'orderkuota.com'
+  APP_PACKAGE: 'app.orderkuota.com:443'
 };
 
 // Mock function to generate dynamic key (replace with actual implementation)
